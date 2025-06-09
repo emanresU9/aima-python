e1c9902b97895d79267a243efe902f3220353411
emanresU9 <wangcc@sunypoly.edu>
2025-06-09 14:20:13 -0400

    First Working Database

--- Extra/The24Puzzle.py
+++ Extra/The24Puzzle.py
@@ -1,5 +1,7 @@
 import math
 from typing import List
+import queue
+import pickle
 
 class X_Puzzle:
     def __init__(self, state):
@@ -32,24 +34,85 @@ class X_Puzzle:
             actions.append(blank+self.side_len)
         return actions
     def result(self, state, action):
-        blank = state.index(0)
-        state[action],state[blank] = state[blank],state[action]
-        return state
-    def create_database(self):
+        new_state = list(state)
+        blank = new_state.index(0)
+        new_state[action],new_state[blank] = new_state[blank],new_state[action]
+        return tuple(new_state)
+    def create_tile_sets(self):
         state = [num for num in range(1,self.size)] + [0]
-        tile_sets = [[-1]*set_count*4 + state[set_count*4:(set_count+1)*4]+[-1]*(len(state)-(set_count+1)*4) for set_count in range(math.floor(self.size/4))]
+        tile_sets = [[-1] * set_count * 4 + state[set_count * 4:(set_count + 1) * 4] + [-1] * (len(state) - (set_count + 1) * 4) for
+         set_count in range(math.floor(self.size / 4))]
         for i in range(len(tile_sets)):
             tile_sets[i][-1] = 0
+            tile_sets[i] = tuple(tile_sets[i])
+        return tile_sets
+    def create_database(self, output):
+        state = [num for num in range(1,self.size)] + [0]
+        tile_sets = self.create_tile_sets()
 
         databases = {tile_set:{} for tile_set in tile_sets}
         for tile_set in tile_sets:
-            databases[tile_set] = IDFS_DB_Create(tile_set)
+            databases[tile_set] = IDS_DB_Create(tile_set)
+    def send_db_to_file(self, database, filename):
+        with open(filename, "wb") as fd:
+            pickle.dump(database, fd)
+
+    def read_db_from_file(self,filename):
+        with open(filename, 'rb') as fd:
+            database = pickle.load(fd)
+        return database or None
 
 
-def IDFS_DB_Create(tile_set):
-    database = {tile_set:0}
-    limit = 1
+    def IDS_DB_Create(tile_set , max_db_size=None):
 
+        def check_add_state(node,database):
+            state = tuple(node["state"])
+            if state in database:
+                return False
+            else:
+                database[state] = node["depth"]
+                return True
+
+        def DLS(puzzle, node, database, limit):
+            if check_add_state(node, database):
+                if len(database) == max_db_size:
+                    return "success"
+            if limit > 0:
+                actions = puzzle.action_list(node['state'])
+                if len(actions) == 0:
+                    return "failure"
+                failure = True
+                for a in actions:
+                    next = dict(state=puzzle.result(node['state'],a), depth=node['depth']+1)
+                    result = DLS(puzzle, next, database, limit-1)
+                    if result == "success":
+                        return "success"
+                    if result != "failure":
+                        failure = False
+                if failure:
+                    return "failure"
+            return "limit"
+
+        if max_db_size is None:
+            max_db_size = math.prod([i for i in range(len(tile_set), len(tile_set) - 5, -1)])
+        puzzle = X_Puzzle(tile_set) #contains transition model methods on the tile_set
+        database = {}
+        limit = 0
+        node = dict(state=tile_set,depth=0)
+        while True:
+            result = DLS(puzzle, node, database, limit)
+            if result == "success":
+                break
+            elif result == "failure":
+                break
+            else:
+                limit += 1
+        return database
 
 puzzle = X_Puzzle([num for num in range(1,16)] + [0])
-state = puzzle.initial
+# state = puzzle.create_tile_sets()[0]
+# database = IDS_DB_Create(state, max_db_size=10)
+# puzzle.send_db_to_file(database, "test.db")
+
+database = puzzle.read_db_from_file("test.db")
+print(sorted(database))
